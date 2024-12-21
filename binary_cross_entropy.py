b3b046f05822e0813fdadadd3401354fe691bf10
allen327lin <allen327lin2@gmail.com>
2024-12-22 06:45:16 +0800

    feat & fix: updated .gitignore and requirements.txt & fixed binary_cross_entropy.py

--- binary_cross_entropy.py
+++ binary_cross_entropy.py
@@ -3,6 +3,12 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import to_categorical
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+# Select only two digits (e.g., 0 and 1)
+DIGIT_1 = 2  # First digit to classify
+DIGIT_2 = 5  # Second digit to classify
 
 
 def binary_cross_entropy(y, yHat):
@@ -15,23 +21,33 @@ if __name__ == "__main__":
     # 載入MNIST資料集
     (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-    # 建立人工神經網路
-    network = Sequential()
-    network.add(Dense(512, activation='relu', input_shape=(784,)))
-    network.add(Dense(10, activation='softmax'))
-    network.compile(optimizer='rmsprop', loss=binary_cross_entropy, metrics=['accuracy'])
-    print(network.summary)
+    # Create masks for the selected digits
+    train_mask = (train_labels == DIGIT_1) | (train_labels == DIGIT_2)
+    test_mask = (test_labels == DIGIT_1) | (test_labels == DIGIT_2)
+
+    # Filter the dataset
+    train_images = train_images[train_mask]
+    train_labels = train_labels[train_mask]
+    test_images = test_images[test_mask]
+    test_labels = test_labels[test_mask]
 
     # 資料前處理
-    train_images = train_images.reshape((60000, 28 * 28))
+    train_images = train_images.reshape((train_images.shape[0], 28 * 28))
     train_images = train_images.astype('float32') / 255
-    test_images = test_images.reshape((10000, 28 * 28))
+    test_images = test_images.reshape((test_images.shape[0], 28 * 28))
     test_images = test_images.astype('float32') / 255
     train_labels = to_categorical(train_labels)
     test_labels = to_categorical(test_labels)
 
+    # 建立人工神經網路
+    network = Sequential()
+    network.add(Dense(512, activation='relu', input_shape=(784,)))
+    network.add(Dense(1, activation='sigmoid'))
+    network.compile(optimizer='rmsprop', loss=binary_cross_entropy, metrics=['accuracy'])
+    print(network.summary)
+
     # 訓練階段
-    network.fit(train_images, train_labels, epochs=5, batch_size=200)
+    network.fit(train_images, train_labels, epochs=10, batch_size=200)
     # 測試階段
     test_loss, test_acc = network.evaluate(test_images, test_labels)
     print("Test Accuracy:", test_acc)
--- cross_entropy.py
+++ cross_entropy.py
@@ -3,6 +3,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import to_categorical
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
 def cross_entropy(y, yHat):
@@ -31,20 +32,20 @@ if __name__ == "__main__":
     # 載入MNIST資料集
     (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
+    # 資料前處理
+    train_images = train_images.reshape((train_images.shape[0], 28 * 28))
+    train_images = train_images.astype('float32') / 255
+    test_images = test_images.reshape((test_images.shape[0], 28 * 28))
+    test_images = test_images.astype('float32') / 255
+    train_labels = to_categorical(train_labels)
+    test_labels = to_categorical(test_labels)
+
     # 建立人工神經網路
     network = Sequential()
     network.add(Dense(512, activation='relu', input_shape=(784,)))
     network.add(Dense(10, activation='softmax'))
     network.compile(optimizer='rmsprop', loss=cross_entropy, metrics=['accuracy'])
     print(network.summary)
-
-    # 資料前處理
-    train_images = train_images.reshape((60000, 28 * 28))
-    train_images = train_images.astype('float32') / 255
-    test_images = test_images.reshape((10000, 28 * 28))
-    test_images = test_images.astype('float32') / 255
-    train_labels = to_categorical(train_labels)
-    test_labels = to_categorical(test_labels)
 
     # 訓練階段
     network.fit(train_images, train_labels, epochs=10, batch_size=200)
